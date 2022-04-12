3c1f5ea96afd53cc3ef31dcad76c74d0929c3769
georgeankumensah <georgeankumensah@gmail.com>
2022-04-12 18:58:12 +0000

    added searchbar functionality

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 // grab the input
 
-document.querySelector(".container-button").addEventListener("click",() => {
+document.querySelector(".container-button").addEventListener("click",(e) => {
     var input = document.querySelector(".container").value;
     console.log(input); 
     pushToDOM(input);
 });
 
-function pushToDOM(input){
+document.querySelector(".container-button").addEventListener('keypress',(e) => {
+    alert("hey I'm working");
+    var input = document.querySelector(".container").value;
+    console.log(input); 
+    if (e.key === 13){
+        pushToDOM(input);
+    }
+});
+
+const pushToDOM = input =>{
     var container = document.querySelector(".data-container");
     container.innerHTML = "<h1 class='txtresult'>Search results for : <span id='result'>"+ input + "</span></h1>";
 }
