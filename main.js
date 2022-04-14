1104336423dacf53355815ab22ab88584976c499
georgeankumensah <georgeankumensah@gmail.com>
2022-04-14 20:59:34 +0000

    added api call and functionality

--- main.js
+++ main.js
@@ -1,27 +1,52 @@
-// grab the input
-
-document.querySelector(".container-button").addEventListener("click",(e) => {
+document.querySelector(".container-button").addEventListener("click", async (e) => {
     var input = document.querySelector(".container").value;
-    console.log(input); 
+    console.log(input);
     pushToDOM(input);
+    await GIPHYAPICALL(input)
 });
 
-document.querySelector(".container-button").addEventListener('keypress',(e) => {
-    alert("hey I'm working");
+document.querySelector(".container").addEventListener('keypress',async (e) => {
     var input = document.querySelector(".container").value;
-    console.log(input); 
-    if (e.key === 13){
+    if (e.which === 13){
         pushToDOM(input);
+        await GIPHYAPICALL(input)
+
     }
 });
 
 const pushToDOM = input =>{
-    var container = document.querySelector(".data-container");
+    var container = document.querySelector(".searchfor");
     container.innerHTML = "<h1 class='txtresult'>Search results for : <span id='result'>"+ input + "</span></h1>";
 }
+var gifBox = document.querySelector(".gifpack");
+const pusher = post => {gifBox.innerHTML += post}
+const clear =()=>{
+    gifBox.innerHTML = ""
+}
+
+// var input = document.querySelector(".container").value;
+const url = "https://api.giphy.com/v1/gifs/search?api_key=PPvQ86bgvMTOX18AREXCwZzdIkXJmmAS&limit=25&offset=0&rating=g&lang=en&q=" 
+
+// const GIPHYAPICALL = require('axios');
+async function GIPHYAPICALL(input){
+    console.log(input)
+ await axios.get(url + input)
+    .then(
+        (response) => {
+            // console.log(response.data[0].images.original.url)
+            console.log(image)
+            const length = response.data.data.length
+            clear()
+            for(let i = 0 ;i < length; i++){
+                var image = response.data.data[i].images.original.url;
+                const post = `<img src=${image}/>`;
+                pusher(post);
+
+            }
+            console.log(post)
+        }
+    )
+    }   
 
 
 
-//  function we(){
-//      alert("done!");
-//  };
