732f79a6afe42ff55e5ad603dc119dff3fc2555a
georgeankumensah <georgeankumensah@gmail.com>
2022-04-15 08:14:03 +0000

    removed all lines containing console.log

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 document.querySelector(".container-button").addEventListener("click", async (e) => {
     var input = document.querySelector(".container").value;
-    console.log(input);
     pushToDOM(input);
     await GIPHYAPICALL(input)
 });
@@ -14,36 +13,36 @@ document.querySelector(".container").addEventListener('keypress',async (e) => {
     }
 });
 
-const pushToDOM = input =>{
+const pushToDOM = input => {
     var container = document.querySelector(".searchfor");
-    container.innerHTML = "<h1 class='txtresult'>Search results for : <span id='result'>"+ input + "</span></h1>";
-}
-var gifBox = document.querySelector(".gifpack");
-const pusher = post => {gifBox.innerHTML += post}
-const clear =()=>{
-    gifBox.innerHTML = ""
+    if (input === ""){
+        container.innerHTML ="";
+    }else{
+        container.innerHTML = "<h1 class='txtresult'>Search results for : <span id='result'>"+ input + "</span></h1>";
+    }
 }
 
-// var input = document.querySelector(".container").value;
+
+var gifBox = document.querySelector(".gifpack");
+const pushAsGif = post => {gifBox.innerHTML += post}
+const clear =()=>{
+    gifBox.innerHTML = "" ;
+}
+
 const url = "https://api.giphy.com/v1/gifs/search?api_key=PPvQ86bgvMTOX18AREXCwZzdIkXJmmAS&limit=25&offset=0&rating=g&lang=en&q=" 
 
-// const GIPHYAPICALL = require('axios');
 async function GIPHYAPICALL(input){
-    console.log(input)
  await axios.get(url + input)
     .then(
         (response) => {
-            // console.log(response.data[0].images.original.url)
-            console.log(image)
             const length = response.data.data.length
             clear()
             for(let i = 0 ;i < length; i++){
                 var image = response.data.data[i].images.original.url;
                 const post = `<img src=${image}/>`;
-                pusher(post);
+                pushAsGif(post);
 
             }
-            console.log(post)
         }
     )
     }   
